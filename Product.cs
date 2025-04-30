364bf00f5f5e9a5f4e184597cc1524a21992468c
A-Iversen <abiv71341@edu.ucl.dk>
2025-04-30 17:18:32 +0200

    Start of Project
    Classes created

---
+++ Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal
+{
+    internal class Product
+    {
+        // Properties - Attributes
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int SKU { get; set; }
+        public string StorageStatus { get; set; }
+        public DateTime DateReceived { get; set; }
+        public double SalePrice { get; set; }
+        public double ImportCost { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public bool Fragile { get; set; }
+
+        // Constructor
+        public Product()
+        {
+            this.Name = Name;
+            this.Category = Category;
+            this.SKU = SKU;
+            this.StorageStatus = StorageStatus;
+            this.DateReceived = DateReceived;
+            this.SalePrice = SalePrice;
+            this.ImportCost = ImportCost;
+            this.Length = Length;
+            this.Width = Width;
+            this.Height = Height;
+            this.Fragile = Fragile;
+        }
+
+
+    }
+}
